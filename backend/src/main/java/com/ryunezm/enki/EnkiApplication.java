0584a78be845f650e78e4423c4f04010f0877cda
ryunezm <61928963+ryunezm@users.noreply.github.com>
2025-06-07 14:20:34 -0500

    Initial commit: monorepo for Enki

---
+++ backend/src/main/java/com/ryunezm/enki/EnkiApplication.java
@@ -0,0 +1,13 @@
+package com.ryunezm.enki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnkiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnkiApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/ryunezm/enki/EnkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryunezm.enki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnkiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
